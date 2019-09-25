354e716228816f33b7adc6714b18ae08a859cda3
mohtth <mtoutouhi@gmail.com>
2019-09-25 10:58:23 +0200

    Testing forms tutorial

---
+++ src/Main.elm
@@ -0,0 +1,94 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+type alias Model =
+  { name : String
+  , password : String
+  , passwordAgain : String
+  , age : Int
+  }
+
+
+init : Model
+init =
+  Model "" "" "" 0
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Name String
+  | Password String
+  | PasswordAgain String
+  | Age String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Name name ->
+      { model | name = name }
+
+    Password password ->
+      { model | password = password }
+
+    PasswordAgain password ->
+      { model | passwordAgain = password }
+
+    Age age ->
+      if String.length age > 0 then
+        case String.toInt age of
+          Just ageInt ->
+            { model | age = ageInt }
+
+          Nothing ->
+            model
+      else
+        { model | age = 0 }
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ viewInput "text" "Name" model.name Name
+    , viewInput "password" "Password" model.password Password
+    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+    , viewInput "age" "Age" (String.fromInt model.age) Age
+    , viewValidation model
+    ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  if model.password == model.passwordAgain
+    && String.length model.password > 8
+    && String.any Char.isDigit model.password
+    && String.any Char.isUpper model.password
+    && String.any Char.isLower model.password
+    then
+    div [ style "color" "green" ] [ text "OK" ]
+  else
+    div [ style "color" "red" ] [ text "Passwords do not match!" ]
